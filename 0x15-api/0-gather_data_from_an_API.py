2e2264db68b5dad6712c63b393f65cb08ea33601
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-09-25 16:49:28 +0100

    Updated

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,18 +8,21 @@ if __name__ == "__main__":
     employee_id = sys.argv[1]
 
     # Fetch user information
-    user_response = requests.get(f"{base_url}users/{employee_id}")
-    user_data = user_response.json()
+    user = requests.get(f"{base_url}users/{employee_id}")
+    user_data = user.json()
     employee_name = user_data.get("name")
 
     # Fetch to-do list
-    todos_response = requests.get(f"{base_url}todos", params={"userId": employee_id})
+    todos_response = requests.get(f"{base_url}todos",
+                                  params={"userId": employee_id})
     todos_data = todos_response.json()
 
     # Filter completed tasks
-    completed_tasks = [task.get("title") for task in todos_data if task.get("completed")]
+    completed_tasks = [task.get("title")
+                       for task in todos_data if task.get("completed")]
 
     # Print information
-    print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{len(todos_data)}):")
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, len(completed_tasks), len(todos_data)))
     for task in completed_tasks:
         print(f"\t {task}")
