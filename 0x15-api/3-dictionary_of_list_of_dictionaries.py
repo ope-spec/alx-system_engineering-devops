99d5a1cf53ab4480660ef49f47a2992c589f22f4
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-09-25 17:13:25 +0100

    Updated

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -21,6 +21,8 @@ if __name__ == "__main__":
                     "task": todo.get("title"),
                     "completed": todo.get("completed"),
                     "username": user.get("username")
-                } for todo in requests.get(f"{base_url}todos", params={"userId": user.get("id")}).json()]
+                }
+                for todo in requests.get(f"{base_url}todos",
+                        params={"userId": user.get("id")}).json()]
             for user in users_data
         }, jsonfile, indent=4)
