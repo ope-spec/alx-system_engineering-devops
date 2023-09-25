b41ea5daee97b9a55bfc5102f52510bd75e1cecb
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-09-25 16:32:15 +0100

    Updated

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
-
 """Fetch and display an employee's task list progress"""
-
 import requests
 import sys
 
 if __name__ == "__main__":
-     employee_id = sys.argv[1]
-     url = f"https://jsonplaceholder.typicode.com/"
-     user = requests.get(f"{url}users/{employee_id}").json()
-     todos = requests.get(f"{url}todos", params={"userId": employee_id}).json()
+    base_url = "https://jsonplaceholder.typicode.com/"
+    employee_id = sys.argv[1]
 
-completed = [t.get("title") for t in todos if t.get("completed") is True]
-print(f"Employee {user.get('name')} is done with tasks({len(completed)}/{len(todos)}):")
-for task in completed:
-    print(f"\t{task}")
+    # Fetch user information
+    user_response = requests.get(f"{base_url}users/{employee_id}")
+    user_data = user_response.json()
+    employee_name = user_data.get("name")
+
+    # Fetch to-do list
+    todos_response = requests.get(f"{base_url}todos", params={"userId": employee_id})
+    todos_data = todos_response.json()
+
+    # Filter completed tasks
+    completed_tasks = [task.get("title") for task in todos_data if task.get("completed")]
+
+    # Print information
+    print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{len(todos_data)}):")
+    for task in completed_tasks:
+        print(f"\t {task}")
