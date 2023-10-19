62189d85733e5ad7368df7b9846a731ece6374f4
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-10-19 13:00:45 +0100

    Tasks done

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,18 +1,21 @@
-# Enhancing Nginx Server Capacity
+# Interpreted on Ubuntu 14.04 LTS
 
 # 1. Create a custom file with the desired ULIMIT value
 file { '/etc/default/nginx_custom':
-  content => 'ulimit -n 4096',
-} ->
+  ensure  => file,
+  content => "ulimit -n 4096\n",
+}
 
 # 2. Copy the custom file over the original Nginx file
 exec { 'copy-nginx-custom':
-  command => '/usr/bin/sudo cp /etc/default/nginx_custom /etc/default/nginx',
-  path    => '/usr/bin:/bin',
-} ->
+  command => '/bin/cp /etc/default/nginx_custom /etc/default/nginx',
+  path    => '/bin',
+  require => File['/etc/default/nginx_custom'],
+}
 
 # 3. Restart Nginx to apply changes
-exec { 'restart-nginx':
-  command => '/usr/bin/sudo service nginx restart',
-  path    => '/etc/init.d/',
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Exec['copy-nginx-custom'],
 }
